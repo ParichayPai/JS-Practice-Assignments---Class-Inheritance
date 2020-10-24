0c1ba0d2909c98d51de96729e32d479eaa3f031d
Parichay Pai <39957257+ParichayPai@users.noreply.github.com>
2020-10-24 18:39:45 +0530

    changes

--- src/components/circle.js
+++ src/components/circle.js
@@ -1,6 +1,11 @@
 // import using require
-
+const shape = require("./shape");
 // declare class
-
+class circle extends shape {
+  calculateArea() {
+    console.log("calculate area in circle");
+  }
+}
 
 // export class using module.exports
+module.exports = circle;
--- src/components/shape.js
+++ src/components/shape.js
@@ -1,3 +1,16 @@
 // declare class
-
+import React from "react";
+class shape extends React.Component {
+  constructor(props) {
+    super(props);
+    this.color = "red";
+  }
+  drawShape() {
+    console.log("drew my shape");
+  }
+  calculateArea() {
+    console.log("calculate area in shape");
+  }
+}
 // export class using module.exports
+module.exports = shape;
